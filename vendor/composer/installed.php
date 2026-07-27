c8fa5e714faa14a51f82995db750379c4b79198f
MTSmalow <medeirosdinieduardo@gmail.com>
2026-07-27 08:09:48 -0300

    feat(api): implementar autenticação, transações e dashboard base

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'project-orbit/finance-api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '99e7f814ab32ac01c8869d0f95532cb29c2fcf25',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'project-orbit/finance-api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '99e7f814ab32ac01c8869d0f95532cb29c2fcf25',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
